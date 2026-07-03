b712fc376a002cce85a685d269fa41bd604ec226
Miguel <miguel@Miguels-MacBook-Air.local>
2026-07-03 19:34:04 +0200

    Add Swift Sim 0.1.0 formula

---
+++ Formula/swift-sim.rb
@@ -0,0 +1,44 @@
+class SwiftSim < Formula
+  desc "Install iOS builds from Codex and preview a Mac Simulator remotely"
+  homepage "https://github.com/Miguelosaurus/Swift-Sim"
+  url "https://github.com/Miguelosaurus/Swift-Sim/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "c12513450697b393ca2f4cbe67cd7948caa012ee36dd89de6973c11dbb590ce1"
+  license "Apache-2.0"
+
+  depends_on "cloudflared"
+  depends_on "node@22"
+
+  def install
+    libexec.install Dir["*"]
+    libexec.install ".agents"
+
+    cd libexec do
+      system Formula["node@22"].opt_bin/"npm", "install", "--omit=dev", "--ignore-scripts"
+    end
+
+    write_launcher "swift-sim", "mac-helper/bin/swift-sim.js"
+    write_launcher "swift-sim-helper", "mac-helper/bin/swift-sim-helper.js"
+  end
+
+  service do
+    run [Formula["node@22"].opt_bin/"node", opt_libexec/"mac-helper/bin/swift-sim-helper.js", "serve"]
+    keep_alive true
+    log_path var/"log/swift-sim.log"
+    error_log_path var/"log/swift-sim.log"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/swift-sim version")
+  end
+
+  private
+
+  def write_launcher(name, script)
+    (bin/name).write <<~SH
+      #!/bin/bash
+      export PATH="#{Formula["node@22"].opt_bin}:#{Formula["cloudflared"].opt_bin}:$PATH"
+      export SWIFT_SIM_MARKETPLACE_ROOT="#{opt_libexec}"
+      exec "#{Formula["node@22"].opt_bin}/node" "#{libexec}/#{script}" "$@"
+    SH
+  end
+end
